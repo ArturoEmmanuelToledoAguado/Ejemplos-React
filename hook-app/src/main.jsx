2d197d9b64bce1856ece1906273110d6d2a03b3b
ArturoEmmanuelToledoAguado <atoledoa1300@alumno.ipn.mx>
2022-12-15 00:25:34 -0600

    useState

--- hook-app/src/main.jsx
+++ hook-app/src/main.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { HooksApp } from './HooksApp'
+import { Counter } from './01-useState/Counter'
+//import { HooksApp } from './HooksApp'
 
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HooksApp />
+    <Counter/>
   </React.StrictMode>,
 )
