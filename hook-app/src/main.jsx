edd591655582bd76150c3a485cff632013ac843e
ArturoEmmanuelToledoAguado <atoledoa1300@alumno.ipn.mx>
2022-12-15 18:54:36 -0600

    Custom Hook

--- hook-app/src/main.jsx
+++ hook-app/src/main.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Counter } from './01-useState/Counter'
+//import { Counter } from './01-useState/Counter'
+import { CounterWithCustomHooks } from './01-useState/CounterWithCustomHooks'
 //import { HooksApp } from './HooksApp'
 
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Counter/>
+    <CounterWithCustomHooks/>
   </React.StrictMode>,
 )
+CounterWithCustomHooks
