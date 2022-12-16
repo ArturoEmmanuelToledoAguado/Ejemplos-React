eaa0a023cb60d097a0ff5cf75976311e178be8e7
ArturoEmmanuelToledoAguado <atoledoa1300@alumno.ipn.mx>
2022-12-15 22:56:57 -0600

    Limpieza

--- hook-app/src/02-useEffect/Message.jsx
+++ hook-app/src/02-useEffect/Message.jsx
@@ -1,19 +1,26 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 export const Message = () => {
 
+    const [coords, setCoords] = useState({x:0,y:0});
+
     useEffect(() => {
-      console.log('Message Mounted')
+        const onMouseMove= ({x,y}) =>{
+            //const coords = {x,y};
+            setCoords({x,y})
+        }
+
+        window.addEventListener('mousemove', onMouseMove);
     
       return () => {
-        console.log('Message UnMonted')
+        window.removeEventListener('mousemove',onMouseMove);
       }
     }, [])
     
-
   return (
     <>
         <h1>Usuario ya existe</h1>
+        {JSON.stringify(coords)}
     </>
   )
 }
