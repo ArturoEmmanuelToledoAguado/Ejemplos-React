327959b1ba9abb1516431ba6f701af58772c82cd
ArturoEmmanuelToledoAguado <atoledoa1300@alumno.ipn.mx>
2022-12-15 23:57:03 -0600

    Formulario con custom Hooks

---
+++ hook-app/src/hooks/useForm.js
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+export const useForm = (initialForm = {}) => {
+    
+    const [formState,setFormSate] = useState(initialForm);
+
+    const onInputChange = ({target}) => {
+        const {name,value} = target;
+        setFormSate({
+            ...formState,
+            [name]:value
+        });
+    }
+  return {
+    ...formState,
+    formState,
+    onInputChange,
+  }
+}
--- hook-app/src/main.jsx
+++ hook-app/src/main.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
 //import { Counter } from './01-useState/Counter'
 //import { CounterWithCustomHooks } from './01-useState/CounterWithCustomHooks'
-import { SimpleForm } from './02-useEffect/SimpleForm'
+//import { SimpleForm } from './02-useEffect/SimpleForm'
 //import { HooksApp } from './HooksApp'
 
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   //<React.StrictMode>
-    <SimpleForm/>
+    <FormWithCustomHook/>
   //</React.StrictMode>,
 )
