138cf37ad2036fc3f5ceec77f0661b0f7c72a08d
ArturoEmmanuelToledoAguado <atoledoa1300@alumno.ipn.mx>
2022-12-15 19:45:27 -0600

    useEffect(simpleForm)

--- hook-app/src/main.jsx
+++ hook-app/src/main.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 //import { Counter } from './01-useState/Counter'
-import { CounterWithCustomHooks } from './01-useState/CounterWithCustomHooks'
+//import { CounterWithCustomHooks } from './01-useState/CounterWithCustomHooks'
+import { SimpleForm } from './02-useEffect/SimpleForm'
 //import { HooksApp } from './HooksApp'
 
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <CounterWithCustomHooks/>
-  </React.StrictMode>,
+  //<React.StrictMode>
+    <SimpleForm/>
+  //</React.StrictMode>,
 )
-CounterWithCustomHooks
