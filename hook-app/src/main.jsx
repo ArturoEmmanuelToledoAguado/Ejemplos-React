ac2126cf04911b4123a79d0cf943eaac4914de25
ArturoEmmanuelToledoAguado <atoledoa1300@alumno.ipn.mx>
2022-12-16 22:34:44 -0600

    useMemo

--- hook-app/src/main.jsx
+++ hook-app/src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { MemoHook } from './06-memos/MemoHook'
 //import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
 //import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
 //import { FocusScreen } from './04-useRef/FocusScreen'
 //import { Layout } from './05-useLAyoutEffect/Layout'
-import { Memorize } from './06-memos/Memorize'
+//import { Memorize } from './06-memos/Memorize'
 //import { Counter } from './01-useState/Counter'
 //import { CounterWithCustomHooks } from './01-useState/CounterWithCustomHooks'
 //import { SimpleForm } from './02-useEffect/SimpleForm'
@@ -14,6 +15,6 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   //<React.StrictMode>
-    <Memorize/>
+    <MemoHook/>
   //</React.StrictMode>,
 )
