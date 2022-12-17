de3e8bfae930209a29b6745a0cacedd7297624c3
ArturoEmmanuelToledoAguado <atoledoa1300@alumno.ipn.mx>
2022-12-16 21:29:07 -0600

    memo

--- hook-app/src/main.jsx
+++ hook-app/src/main.jsx
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client'
 //import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
 //import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
 //import { FocusScreen } from './04-useRef/FocusScreen'
-import { Layout } from './05-useLAyoutEffect/Layout'
+//import { Layout } from './05-useLAyoutEffect/Layout'
+import { Memorize } from './06-memos/Memorize'
 //import { Counter } from './01-useState/Counter'
 //import { CounterWithCustomHooks } from './01-useState/CounterWithCustomHooks'
 //import { SimpleForm } from './02-useEffect/SimpleForm'
@@ -13,6 +14,6 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   //<React.StrictMode>
-    <Layout/>
+    <Memorize/>
   //</React.StrictMode>,
 )
