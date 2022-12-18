97b35d0c503835fb672c08973d5d9259a0f346a2
ArturoEmmanuelToledoAguado <atoledoa1300@alumno.ipn.mx>
2022-12-18 00:42:28 -0600

    useReducer

--- hook-app/src/main.jsx
+++ hook-app/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 //import { CallbackHook } from './06-memos/CallbackHook'
-import { Padre } from './07-tarea-memo/Padre'
+//import { Padre } from './07-tarea-memo/Padre'
 //import { MemoHook } from './06-memos/MemoHook'
 //import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
 //import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
@@ -14,11 +14,12 @@ import { Padre } from './07-tarea-memo/Padre'
 //import { HooksApp } from './HooksApp'
 
 import './08-useReducer/intro-reducer'
+import { TodoApp } from './08-useReducer/TodoApp'
 
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   //<React.StrictMode>
-    //<Padre/>
+    <TodoApp/>
   //</React.StrictMode>,
 )
