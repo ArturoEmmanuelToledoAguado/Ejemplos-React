2ac97fbb91a08c6eda1cd95fbc813be250c45378
ArturoEmmanuelToledoAguado <atoledoa1300@alumno.ipn.mx>
2022-12-19 21:25:23 -0600

    Borrar un TODO

--- hook-app/src/08-useReducer/TodoApp.jsx
+++ hook-app/src/08-useReducer/TodoApp.jsx
@@ -35,6 +35,13 @@ export const TodoApp = () => {
         dispatch(action)
     }
 
+    const handleDeleteTodo = (id) => {
+        dispatch({
+            type: '[TODO] Remove Todo',
+            payload: id
+        });
+    }
+
   return (
     <>
         <h1>TodoApp:10,<small>Pendientes:2</small></h1>
@@ -42,7 +49,10 @@ export const TodoApp = () => {
 
         <div className="row">
             <div className="col-7">
-                <TodoList todos={todos}/>
+                <TodoList 
+                    todos={todos} 
+                    onDeleteTodo={handleDeleteTodo}
+                />
             </div>
 
             <div className="col-5">
